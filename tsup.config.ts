a425fd225560e40d831971c300514eaaa61f6cf3
suraj maurya <mnaveen1888@gmail.com>
2025-02-14 13:50:18 +0530

    removed uneccessary dependency

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   entry: ["src/index.ts"],  // Single entry point
   format: ["cjs", "esm"],
   dts: true,
-  sourcemap: true,
+  sourcemap: true, // Add sourcemap for debugging
   clean: true,
+  treeshake: true,          // Remove unused code
+  minify: true,             // Minify output
+  splitting: false,         // Avoid splitting for SDKs
 });
